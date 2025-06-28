7675f0d85ae4f799a46d5be295bd919f7ff62219
Oscar Higgott <oscarhiggott@google.com>
2025-06-28 21:10:03 +0000

    format, add workflow, add full options for ldpc v2 bp, expose these to sinter decoder, add ci

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='beliefmatching',
-    version=version['__version__'],
+    name="beliefmatching",
+    version=version["__version__"],
     packages=find_packages("src"),
     url="https://github.com/oscarhiggott/BeliefMatching",
     description="A package for decoding quantum error correcting codes using belief-matching.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    package_dir={'': 'src'},
-    author='Oscar Higgott',
-    install_requires=['stim', 'pymatching>=2.0.1', 'ldpc', 'sinter>=1.11']
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    author="Oscar Higgott",
+    install_requires=["stim", "pymatching>=2.0.1", "ldpc", "sinter>=1.11"],
 )
--- src/beliefmatching/__init__.py
+++ src/beliefmatching/__init__.py
@@ -1,2 +1,17 @@
-from .belief_matching import *
+from .belief_matching import (
+    iter_set_xor,
+    dict_to_csc_matrix,
+    DemMatrices,
+    detector_error_model_to_check_matrices,
+    BeliefMatching,
+)
 from .sinter_belief_matching import BeliefMatchingSinterDecoder
+
+__all__ = [
+    "iter_set_xor",
+    "dict_to_csc_matrix",
+    "DemMatrices",
+    "detector_error_model_to_check_matrices",
+    "BeliefMatching",
+    "BeliefMatchingSinterDecoder",
+]
